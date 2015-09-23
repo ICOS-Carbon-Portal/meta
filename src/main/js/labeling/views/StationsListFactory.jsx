eab48085d7495646089f328f310a4fd99140c692
Paul <paul.hedberg@nateko.lu.se>
2015-09-23 13:49:17 +0200

    A demo of station labeling is ready.

--- src/main/js/labeling/views/StationsListFactory.jsx
+++ src/main/js/labeling/views/StationsListFactory.jsx
@@ -1,27 +1,32 @@
 module.exports = function(stationListStore, chooseObjectAction){
+
+	var Station = require('./Station.jsx')();
 	
 	return React.createClass({
-	
+		
 		mixins: [Reflux.connect(stationListStore)],
 
 		render: function(){
-
-console.log(this.state.chosen);
+			
+			var self = this;
 
 			return <ul>{
-				this.state.stations.map(function(station){
+				self.state.stations.map(function(station){
 
 					var chooseObjectHandler = () => chooseObjectAction(station);
+
 					var editStatus = station.isUsersStation ? 'editable' : 'not editable';
 
-					var panelClasses = 'panel ' + (station.chosen ? 'primary' : 'panel-default');
+					var panelClasses = 'panel ' + (station.chosen ? 'panel-primary' : 'panel-default');
 					var panelBodyStyle = {display: (station.chosen ? 'block' : 'none')};
 
-					return <li key={station.uri} style={{"list-style-type": "none", "padding-bottom": "10px"}}>
+					return <li key={station.stationUri} style={{"list-style-type": "none", "padding-bottom": "10px"}}>
 
 						<div className={panelClasses}>
-							<div className="panel-heading" onClick={chooseObjectHandler}>{[station.longName, ' (', station.theme, ') ', editStatus].join('')}</div>
-							<div className="panel-body" style={panelBodyStyle}></div>
+							<div className="panel-heading" onClick={chooseObjectHandler}>{[station.longName, ' (', station.theme, ') '].join('')}</div>
+							<div className="panel-body" style={panelBodyStyle}>
+								{ (self.state.chosen && station.chosen) ? <Station station={self.state.chosen} theme={station.theme} edit={editStatus} /> : null }
+							</div>
 						</div>
 
 					</li>;

73eced4971bd7311ee95e16ef9bd249d7738f37a
Roger Groth <roger.groth@gis.lu.se>
2015-11-06 10:44:40 +0100

    Expanded on the mail sender

--- src/main/java/se/lu/nateko/cp/meta/services/labeling/SendMail.java
+++ src/main/java/se/lu/nateko/cp/meta/services/labeling/SendMail.java
@@ -5,6 +5,9 @@ import org.apache.commons.mail.*;
 
 public class SendMail {
 
+	private static final String smtpServer = "mail.lu.se";
+	private static final String adminMailAddress = "carbon.admin@nateko.lu.se";
+
 	public static void sendMail(String from, String[] to, String subject, String body, boolean isHtml, String[] cc, String[] bcc){
 		if (isHtml){
 			sendHtml(from, to, subject, body, cc, bcc);
@@ -33,10 +36,14 @@ public class SendMail {
 		sendTxt(from, to, subject, body, new String[0], new String[0]);
 	}
 
+	public static void sendSystemMail(String subject, String body){
+		sendTxt(adminMailAddress, new String[]{adminMailAddress}, subject, body, new String[0], new String[0]);
+	}
+
 	private static void sendTxt(String from, String[] to, String subject, String body, String[] cc, String[] bcc){
 		Email email = new SimpleEmail();
-		email.setHostName("mail.lu.se");
-		email.setBounceAddress("carbon.admin@nateko.lu.se");
+		email.setHostName(smtpServer);
+		email.setBounceAddress(adminMailAddress);
 
 		try {
 			email.setFrom(from);
@@ -62,8 +69,8 @@ public class SendMail {
 
 	private static void sendHtml(String from, String[] to, String subject, String body, String[] cc, String[] bcc){
 		HtmlEmail email = new HtmlEmail();
-		email.setHostName("mail.lu.se");
-		email.setBounceAddress("carbon.admin@nateko.lu.se");
+		email.setHostName(smtpServer);
+		email.setBounceAddress(adminMailAddress);
 
 		try {
 			email.setFrom(from);

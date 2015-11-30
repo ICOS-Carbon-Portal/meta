2d0a6d0eefe0ee2377d262630d70cb9e76ecf557
Roger Groth <roger.groth@gis.lu.se>
2015-11-30 14:34:22 +0100

    SPARQL client now handles urls to specific queries and types

--- src/main/js/sparqlclient/main.js
+++ src/main/js/sparqlclient/main.js
@@ -1,6 +1,6 @@
-function availableRequests(){
+function availableQueries(){
 	var queries = [{
-		name:"Get all properties",
+		name:"Get all property names",
 		query:
 `SELECT DISTINCT ?property
 FROM <http://meta.icos-cp.eu/ontologies/stationentry/>
@@ -38,7 +38,32 @@ OPTIONAL{?pi cpst:hasFirstName ?piFname } .
 ?s cpst:hasSiteType ?siteType .
 }
 GROUP BY ?s ?lat ?lon ?spatRef ?locationDesc ?class ?country ?sName ?lName ?siteType`
-		}];
+		},
+		{
+			name:"Get labelings",
+			query:
+`PREFIX cpst: <http://meta.icos-cp.eu/ontologies/stationentry/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT *
+FROM <http://meta.icos-cp.eu/ontologies/stationsschema/>
+FROM <http://meta.icos-cp.eu/ontologies/stationentry/>
+FROM NAMED <http://meta.icos-cp.eu/ontologies/stationlabeling/>
+WHERE{
+	?owlClass rdfs:subClassOf cpst:Station .
+	?s a ?owlClass .
+	?s cpst:hasPi ?pi .
+	?s cpst:hasShortName ?provShortName .
+	?s cpst:hasLongName ?provLongName .
+	OPTIONAL{
+		GRAPH <http://meta.icos-cp.eu/ontologies/stationlabeling/> {
+			OPTIONAL {?s cpst:hasShortName ?hasShortName }
+			OPTIONAL {?s cpst:hasLongName ?hasLongName }
+			OPTIONAL {?s cpst:hasApplicationStatus ?hasApplicationStatus }
+		}
+	}
+}`
+		}
+	];
 
 	return queries;
 }
@@ -144,18 +169,6 @@ function syntaxHighlight(json) {
 	});
 }
 
-function setReturnType(selectedType){
-	$("#returnTypeGrp").children().each(function (index){
-		var $this = $(this);
-
-		if($this.text() == selectedType){
-			$this.addClass("active");
-		} else {
-			$this.removeClass("active");
-		}
-	});
-}
-
 function selectText(containerid) {
 	if (document.selection) {
 		var range = document.body.createTextRange();
@@ -169,7 +182,6 @@ function selectText(containerid) {
 }
 
 function toggleSubmitBtn(contentLength){
-	console.log(contentLength);
 	if (contentLength > 0){
 		$("#queryBtn").prop("disabled", false);
 	} else {
@@ -177,19 +189,112 @@ function toggleSubmitBtn(contentLength){
 	}
 }
 
-$(function () {
-	$("#returnTypeGrp").children().each(function (index){
+function processQuery(paramsEnc){
+	// paramsEnc starts with "?"
+	var params = decodeURI(paramsEnc.substring(1));
+	var pairsArr = params.split("&");
+	var query = {};
+
+	pairsArr.forEach(function(pair){
+		var keyVal = pair.split("=");
+		query[keyVal[0]] = keyVal[1];
+	});
+
+	return query;
+}
+
+function updateUrlBar(state){
+	var query = "?";
+
+	if(state.q !== undefined){
+		query += "q=" + state.q + "&";
+	}
+
+	query += "type=" + state.type;
+
+	history.pushState(state, "", query);
+};
+
+function initRequestDdl(queries, queryParams, state){
+	var $ddl = $("#selReqDdl");
+
+	for (var ind in queries){
+		$ddl.append($("<option />").val(queries[ind].query).text(queries[ind].name));
+	};
+
+	$ddl.change(function(){
+		if ($ddl.val() != "0") {
+			$("#queryTA").val($ddl.val());
+			toggleSubmitBtn(1);
+
+			state.q = $ddl.children("option").filter(":selected").text();
+			updateUrlBar(state);
+		}
+	});
+
+	if (queryParams.q !== undefined){
+		$ddl.children("option").each(function() {
+			if($(this).text() == queryParams.q) {
+				$(this).attr('selected', 'selected');
+				$ddl.change();
+				state.q = queryParams.q;
+			}
+		});
+	}
+
+	return state;
+}
+
+function setReturnType($btns, selectedType){
+	$btns.children().each(function (index){
 		var $this = $(this);
 
-		if(index == 0){
+		if($this.text() == selectedType){
 			$this.addClass("active");
+		} else {
+			$this.removeClass("active");
+		}
+	});
+}
+
+function initReturnTypeBtns(queryParams, state){
+	var $btns = $("#returnTypeGrp");
+
+	$btns.children().each(function (index){
+		var $this = $(this);
+
+		if(index == 1){
+			$this.addClass("active");
+			state.type = $this.text();
 		}
 
 		$this.click(function(){
-			setReturnType($this.text());
+			setReturnType($btns, $this.text());
 		})
 	});
 
+	$btns.children().each(function(ind, child){
+		$(child).click(function(){
+			state.type = $(child).text();
+			updateUrlBar(state);
+		});
+	});
+
+	if (queryParams.type !== undefined){
+		$btns.children().each(function(ind, child){
+			$(child).removeClass("active");
+
+			if ($(child).text() == queryParams.type){
+				$(child).addClass("active");
+				state.type = queryParams.type;
+			}
+		});
+	}
+
+	updateUrlBar(state);
+}
+
+$(function () {
 	$("#copyTxtBtn").hide();
 	$("#copyTxtBtn").click(function(){
 		selectText("result");
@@ -211,17 +316,13 @@ $(function () {
 		toggleSubmitBtn(this.value.length);
 	});
 
-	var queries = availableRequests();
-	var $ddl = $("#selReqDdl");
-
-	for (var ind in queries){
-		$ddl.append($("<option />").val(queries[ind].query).text(queries[ind].name));
+	var queries = availableQueries();
+	var queryParams = processQuery(window.location.search);
+	var state = {
+		q: undefined,
+		type: undefined
 	};
 
-	$ddl.change(function(){
-		if ($ddl.val() != "0") {
-			$("#queryTA").val($ddl.val());
-			toggleSubmitBtn(1);
-		}
-	});
+	state = initRequestDdl(queries, queryParams, state);
+	initReturnTypeBtns(queryParams, state);
 });

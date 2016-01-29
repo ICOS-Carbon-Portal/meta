5c7bd52e9d8c32a4516a5cbabbaa8f8f72980646
Roger Groth <roger.groth@gis.lu.se>
2016-01-29 11:56:56 +0100

    Landing page: added position and theme

--- src/main/scala/se/lu/nateko/cp/meta/services/upload/DataObjectFetcher.scala
+++ src/main/scala/se/lu/nateko/cp/meta/services/upload/DataObjectFetcher.scala
@@ -30,15 +30,15 @@ class DataObjectFetcher(server: InstanceServer, pidFactory: Sha256Sum => String)
 
 		val production: URI = getSingleUri(dataObjUri, vocab.wasProducedBy)
 		val producer: URI = getSingleUri(production, vocab.prov.wasAssociatedWith)
-		val producerName: String = getSingleString(producer, vocab.hasName).get
+		val producerName: Option[String] = getSingleString(producer, vocab.hasName)
 
 		val prodStart = getSingleInstant(production, vocab.prov.startedAtTime).get
 		val prodStop = getSingleInstant(production, vocab.prov.endedAtTime).get
-		val posLat: Double = getSingleDouble(producer, vocab.hasLongitude).getOrElse(0)
+		val pos = getPos(production)
 
 		val submission: URI = getSingleUri(dataObjUri, vocab.wasSubmittedBy)
 		val submitter: URI = getSingleUri(submission, vocab.prov.wasAssociatedWith)
-		val submitterName: String = getSingleString(submitter, vocab.hasName).get
+		val submitterName: Option[String] = getSingleString(submitter, vocab.hasName)
 
 		val submStart = getSingleInstant(submission, vocab.prov.startedAtTime).get
 		val submStop = getSingleInstant(submission, vocab.prov.endedAtTime)
@@ -55,20 +55,20 @@ class DataObjectFetcher(server: InstanceServer, pidFactory: Sha256Sum => String)
 			fileName = fileName,
 			pid = submStop.map(_ => pidFactory(hash)),
 			production = DataProduction(
-				theme = ThemeAS,
+				theme = getTheme(producer),
 				producer = UriResource(
 					uri = producer,
-					label = Some(producerName)
+					label = producerName
 				),
 				start = prodStart,
 				stop = prodStop,
-				pos = Some(Map("lat" -> 1.2, "lon" -> 3)),
+				pos = pos,
 				coverage = None
 			),
 			submission = DataSubmission(
 				submitter = UriResource(
 					uri = submitter,
-					label = Some(submitterName)
+					label = submitterName
 				),
 				start = submStart,
 				stop = submStop
@@ -83,6 +83,26 @@ class DataObjectFetcher(server: InstanceServer, pidFactory: Sha256Sum => String)
 
 	private def getStatus(submStop: Option[Instant]): DataObjectStatus =
 		if(submStop.isDefined) UploadOk else NotComplete
+
+	private def getTheme(subj: URI): ProducerTheme = {
+		val themes = server.getValues(subj, RDF.TYPE).collect{
+			case uri: URI if(uri == vocab.atmoStationClass) => ThemeAS
+			case uri: URI if(uri == vocab.ecoStationClass) => ThemeES
+			case uri: URI if(uri == vocab.oceStationClass) => ThemeOS
+		}
+		assert(themes.size == 1, s"Expected $subj to be a station of exactly one theme but got ${themes.size}!")
+		themes.head
+	}
+
+	private def getPos(subj: URI): Option[Map[String, Double]] = {
+		val posLat: Option[Double] = getSingleDouble(subj, vocab.hasLatitude)
+		val posLon: Option[Double] = getSingleDouble(subj, vocab.hasLongitude)
+
+		(posLat, posLon) match {
+			case (Some(posLat), Some(posLon)) => Some(Map("lat" -> posLat, "lon" -> posLon))
+			case _ => None
+		}
+	}
 
 	private def getSingleUri(subj: URI, pred: URI): URI = {
 		val vals = server.getValues(subj, pred).collect{

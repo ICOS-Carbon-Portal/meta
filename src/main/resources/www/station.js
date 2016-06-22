ff185257f1175e5723b21f6c48d0bec0954284ba
Roger Groth <roger.groth@gis.lu.se>
2016-06-22 14:17:03 +0200

    Added base map switch control and masked polygons for landing page map

--- src/main/resources/www/station.js
+++ src/main/resources/www/station.js
@@ -11,51 +11,90 @@ function initMap(queryParams) {
 		var geoJson = JSON.parse(queryParams.coverage);
 		var map = L.map(mapDiv);
 
-		L.tileLayer('http://server.arcgisonline.com/arcgis/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-			maxZoom: 18
-		}).addTo(map);
+		var baseMaps = getBaseMaps(18);
+		map.addLayer(baseMaps.Topographic);
+		L.control.layers(baseMaps).addTo(map);
 
-		var fg = new L.FeatureGroup();
-		var icon = getIcon(queryParams.theme);
-		// var marker = getMarker(queryParams.theme);
+		if (geoJson.type == "Polygon"){
+			L.Mask = L.Polygon.extend({
+				options: {
+					stroke: false,
+					color: '#333',
+					fillOpacity: 0.5,
+					clickable: false,
+					outerBounds: new L.LatLngBounds([-90, -360], [90, 360])
+				},
 
-		fg.addLayer(L.geoJson(geoJson, {
-			pointToLayer: function (feature, latlng) {
-				// return L.circleMarker(latlng, marker);
-				return L.marker(latlng, {icon});
-			},
-			style: function(feature){
-				switch(feature.geometry.type){
-					case 'Line':
-						return {color: "rgb(50,50,255)", weight: 2};
-						break;
+				initialize: function (latLngs, options) {
+					var outerBoundsLatLngs = [
+						this.options.outerBounds.getSouthWest(),
+						this.options.outerBounds.getNorthWest(),
+						this.options.outerBounds.getNorthEast(),
+						this.options.outerBounds.getSouthEast()
+					];
+					L.Polygon.prototype.initialize.call(this, [outerBoundsLatLngs, latLngs], options);
+				},
 
-					case 'LineString':
-						return {color: "rgb(50,50,255)", weight: 2};
-						break;
+			});
 
-					case 'MultiLineString':
-						return {color: "rgb(50,50,255)", weight: 2};
-						break;
+			L.mask = function (latLngs, options) {
+				return new L.Mask(latLngs, options);
+			};
 
-					case 'Polygon':
-						return {color: "rgb(255,50,50)", weight: 2};
-						break;
+			var mask = getMask(geoJson);
+			L.mask(mask).addTo(map);
+			map.fitBounds(mask);
 
-					default:
-						return {color: "rgb(50,255,50)", weight: 2};
-				}
-			}
-		}));
-
-		map.addLayer(fg);
-
-		if (geoJson.type == "Point"){
-			map.setView([geoJson.coordinates[1], geoJson.coordinates[0]], 3);
 		} else {
-			map.fitBounds(fg.getBounds());
+			var fg = new L.FeatureGroup();
+			var icon = getIcon(queryParams.theme);
+			// var marker = getMarker(queryParams.theme);
+
+			fg.addLayer(L.geoJson(geoJson, {
+				pointToLayer: function (feature, latlng) {
+					// return L.circleMarker(latlng, marker);
+					return L.marker(latlng, {icon});
+				},
+				style: function (feature) {
+					switch (feature.geometry.type) {
+						case 'Line':
+							return {color: "rgb(50,50,255)", weight: 2};
+							break;
+
+						case 'LineString':
+							return {color: "rgb(50,50,255)", weight: 2};
+							break;
+
+						case 'MultiLineString':
+							return {color: "rgb(50,50,255)", weight: 2};
+							break;
+
+						default:
+							return {color: "rgb(50,255,50)", weight: 2};
+					}
+				}
+			}));
+
+			map.addLayer(fg);
+
+			if (geoJson.type == "Point") {
+				map.setView([geoJson.coordinates[1], geoJson.coordinates[0]], 4);
+			} else {
+				map.fitBounds(fg.getBounds());
+			}
 		}
 	}
+}
+
+function getMask(geoJson){
+	var coordinates = geoJson.coordinates[0];
+	var latLngs = [];
+
+	coordinates.forEach(function(coord){
+		latLngs.push(new L.LatLng(coord[1], coord[0]));
+	});
+
+	return latLngs;
 }
 
 function getIcon(theme){
@@ -130,6 +169,32 @@ function getMarker(theme){
 	}
 
 	return marker;
+}
+
+function getBaseMaps(maxZoom){
+	var topo = L.tileLayer(window.location.protocol + '//server.arcgisonline.com/arcgis/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+		maxZoom
+	});
+
+	var image = L.tileLayer(window.location.protocol + '//server.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+		maxZoom
+	});
+
+	var osm = L.tileLayer(window.location.protocol + "//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+		maxZoom
+	});
+
+	var mapQuest = L.tileLayer("http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png", {
+		maxZoom,
+		subdomains: "1234"
+	});
+
+	return {
+		"Topographic": topo,
+		"Satellite": image,
+		"OSM": osm,
+		"MapQuest": mapQuest
+	};
 }
 
 function processQuery(paramsEnc) {

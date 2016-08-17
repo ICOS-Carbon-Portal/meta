98f66496a037f6005a67b02addf1a14b0cd6fe48
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2016-08-17 10:29:05 +0200

    Updated the SPARQL query for the provisional stations' PIs

--- src/main/js/sparqlclient/queries/stationPis.js
+++ src/main/js/sparqlclient/queries/stationPis.js
@@ -1,21 +1,24 @@
+const stationEntryUriPrefix = "http://meta.icos-cp.eu/resources/stationentry/";
+const stationEntryOntUriPrefix = "http://meta.icos-cp.eu/ontologies/stationentry/";
 
-export const stationPis = `prefix st: <http://meta.icos-cp.eu/ontologies/stationentry/>
-select distinct ?stationId ?stationName ?firstName ?lastName ?email
-from <http://meta.icos-cp.eu/resources/stationentry/>
+export const stationPis = `prefix st: <${stationEntryOntUriPrefix}>
+select distinct ?stationTheme ?stationId ?stationName ?firstName ?lastName ?email
+from <${stationEntryUriPrefix}>
 where{
-  ?s a st:ES .
   ?s st:hasShortName ?stationId .
   ?s st:hasLongName ?stationName .
   ?s st:hasPi ?pi .
   ?pi st:hasFirstName ?firstName .
   ?pi st:hasLastName ?lastName .
   ?pi st:hasEmail ?email .
+  ?s a ?stationClass .
+  BIND (replace(str(?stationClass), "${stationEntryOntUriPrefix}", "") AS ?stationTheme )
 }
-order by ?lastName`;
+order by UCASE(?lastName) ?firstName ?stationId`;
 
-export const etcClass1And2Pis = `prefix st: <http://meta.icos-cp.eu/ontologies/stationentry/>
+export const etcClass1And2Pis = `prefix st: <${stationEntryOntUriPrefix}>
 select distinct ?stationId ?stationClass ?firstName ?lastName ?email
-from <http://meta.icos-cp.eu/resources/stationentry/>
+from <${stationEntryUriPrefix}>
 where{
   ?s a st:ES .
   ?s st:hasShortName ?stationId .

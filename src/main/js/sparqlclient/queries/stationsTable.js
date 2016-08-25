1353d019b9289987f2d98c9282ad5219735ba983
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2016-08-25 13:18:20 +0200

    Updated SPARQL query for the 'table' of provisional stations

--- src/main/js/sparqlclient/queries/stationsTable.js
+++ src/main/js/sparqlclient/queries/stationsTable.js
@@ -1,10 +1,8 @@
 
 export const stationsTable = `PREFIX cpst: <http://meta.icos-cp.eu/ontologies/stationentry/>
 SELECT
-(str(?s) AS ?id)
 (IF(bound(?lat), str(?lat), "?") AS ?latstr)
 (IF(bound(?lon), str(?lon), "?") AS ?lonstr)
-(IF(bound(?spatRef), str(?spatRef), "?") AS ?geoJson)
 (REPLACE(str(?class),"http://meta.icos-cp.eu/ontologies/stationentry/", "") AS ?themeShort)
 (str(?country) AS ?Country)
 (str(?sName) AS ?Short_name)
@@ -13,16 +11,16 @@ SELECT
 (str(?siteType) AS ?Site_type)
 FROM <http://meta.icos-cp.eu/resources/stationentry/>
 WHERE {
+?s cpst:hasCountry ?country .
+?s cpst:hasShortName ?sName .
+?s cpst:hasLongName ?lName .
+?s cpst:hasSiteType ?siteType .
+?s cpst:hasPi ?pi .
+?pi cpst:hasLastName ?piLname .
 ?s a ?class .
 OPTIONAL{?s cpst:hasLat ?lat } .
 OPTIONAL{?s cpst:hasLon ?lon } .
 OPTIONAL{?s cpst:hasSpatialReference ?spatRef } .
-?s cpst:hasCountry ?country .
-?s cpst:hasShortName ?sName .
-?s cpst:hasLongName ?lName .
-?s cpst:hasPi ?pi .
 OPTIONAL{?pi cpst:hasFirstName ?piFname } .
-?pi cpst:hasLastName ?piLname .
-?s cpst:hasSiteType ?siteType .
 }
-GROUP BY ?s ?lat ?lon ?spatRef ?locationDesc ?class ?country ?sName ?lName ?siteType`;
+GROUP BY ?lat ?lon ?class ?country ?sName ?lName ?siteType`;

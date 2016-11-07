efcc01912ee69159418b254232e4d2110f8d76ae
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2016-11-07 13:51:53 +0100

    Switched most of the pages to the master template

--- src/main/js/labeling/views/NavBarFactory.jsx
+++ src/main/js/labeling/views/NavBarFactory.jsx
@@ -7,27 +7,20 @@ module.exports = function(WhoAmIStore) {
 		render: function() {
 			var loggedIn = (this.state.user.mail !== 'dummy@dummy.none');
 
-			var title = this.props.piMode
-				? "PI Information"
-				: "ICOS Station Labeling";
-
-			return <nav className="navbar navbar-default navbar-static-top">
-				<div className="container-fluid">
-					<div className="navbar-right" style={{marginTop: 7, marginRight: 3}}>
-						{loggedIn ? <p>Logged in as {this.state.user.mail}</p> : <p>Not logged in</p>}
-						{loggedIn ? null : <p>Log in <a href="https://cpauth.icos-cp.eu/login/?targetUrl=http://meta.icos-cp.eu/labeling/">here</a></p>}
-						{this.state.user.isPi
-							? this.props.piMode
-								? <p>Back to <a href="#">station labeling</a></p>
-								: <p>Edit your info <a href="#piinfo">here</a></p>
-							: null}
-					</div>
-					<div className="navbar-header">
-						<img alt="Carbon Portal" src="https://static.icos-cp.eu/images/Icos_Logo_RGB_White_Carbon_Portal_80px.png" />
-					</div>
-					<h1>{title}</h1>
+			return <div className="page-header">
+				<div className="pull-right">
+					{loggedIn ? <p>Logged in as {this.state.user.mail}</p> : <p>Not logged in</p>}
+					{loggedIn ? null : <p>Log in <a href="https://cpauth.icos-cp.eu/login/?targetUrl=http://meta.icos-cp.eu/labeling/">here</a></p>}
+					{this.state.user.isPi
+						? this.props.piMode
+							? <p>Back to <a href="#">station labeling</a></p>
+							: <p>Edit your info <a href="#piinfo">here</a></p>
+						: null}
 				</div>
-			</nav>;
+				{this.props.piMode
+					? <h1>PI Information <small>for ICOS metadata</small></h1>
+					: <h1>ICOS Station Labeling <small>Step 1</small></h1>}
+			</div>;
 		}
 
 	});

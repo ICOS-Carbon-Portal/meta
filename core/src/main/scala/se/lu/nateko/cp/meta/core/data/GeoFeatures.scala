d502d57242efb4b3b17af3148817477427fc1a25
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2017-02-21 14:54:54 +0100

    Textual coverage and custom HTML page titles to landing pages

--- core/src/main/scala/se/lu/nateko/cp/meta/core/data/GeoFeatures.scala
+++ core/src/main/scala/se/lu/nateko/cp/meta/core/data/GeoFeatures.scala
@@ -2,11 +2,13 @@ package se.lu.nateko.cp.meta.core.data
 
 sealed trait GeoFeature{
 	def geoJson: String
+	def textSpecification: String
 }
 
 object GeoFeature{
 	def apply(geojson: String) = new GeoFeature{
 		def geoJson = geojson
+		def textSpecification = geojson
 	}
 }
 
@@ -15,6 +17,8 @@ case class Position(lat: Double, lon: Double) extends GeoFeature{
 	|	"type": "Point",
 	|	"coordinates": [$lon, $lat]
 	}""".stripMargin
+
+	def textSpecification = s"Lat: $lat, Lon: $lon"
 }
 
 case class SpatialCoverage(min: Position, max: Position, label: Option[String]) extends GeoFeature{
@@ -24,4 +28,6 @@ case class SpatialCoverage(min: Position, max: Position, label: Option[String]) 
 	|		[[${min.lon}, ${min.lat}], [${min.lon}, ${max.lat}], [${max.lon}, ${max.lat}], [${max.lon}, ${min.lat}], [${min.lon}, ${min.lat}]]
 	|	]
 	|}""".stripMargin
+
+	def textSpecification = s"S: ${min.lat}, W: ${min.lon}, N: ${max.lat}, E: ${max.lon}"
 }

fe7dea400cca9d21b9bde0a6aeefb26cab1b1bff
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2018-03-01 16:24:43 +0100

    ATC-flavoured landing pages

---
+++ src/main/scala/views/html/AtcStyleConfig.scala
@@ -0,0 +1,14 @@
+package views.html
+
+import se.lu.nateko.cp.viewscore.IcosStyleConfig
+import java.net.URI
+
+object AtcStyleConfig extends IcosStyleConfig{
+	private val prefix = "https://static.icos-cp.eu/images/atc/"
+
+	val headerImage = new URI(prefix + "icos-atc_header.jpg")
+	val headerImageMedium = new URI(prefix + "icos-atc_header_medium.jpg")
+	val headerImageSmall = new URI(prefix + "icos-atc_header_small.jpg")
+	val headerLogo = new URI(prefix + "atc_logo_opt.svg")
+	val headerHomeLink = new URI("http://www.icos-atc.eu/")
+}

191ea98f3290b68249d21f4e4c83f99182e280ea
Roger Groth <roger.groth@gis.lu.se>
2018-04-24 14:07:24 +0200

    [labeling-status] Ignore stations not found in sparql result

--- src/main/python/labeling-status/labelingStatus.py
+++ src/main/python/labeling-status/labelingStatus.py
@@ -17,17 +17,8 @@ for line in sparql_result.splitlines():
 rdf_query_file = os.path.join(os.getcwd(), 'rdf-query.sql')
 rdf_log = check_output("ssh fsicos.lunarc.lu.se docker exec -i rdflogdb-v9.6 psql -U postgres -A -F ',' -t -d postgres < " + rdf_query_file, shell=True)
 
-non_existing_urls = []
-
 for line in rdf_log.splitlines():
 	date, url, status = line.decode("utf-8").split(',')
 
 	if url in lookup:
 		print (','.join((date, lookup[url][0], lookup[url][1], status)))
-	else:
-		non_existing_urls.append(url)
-
-if (len(non_existing_urls) > 0):
-	print('-' * 150)
-	print("Stations not yet labelled:")
-	print(', '.join(non_existing_urls))

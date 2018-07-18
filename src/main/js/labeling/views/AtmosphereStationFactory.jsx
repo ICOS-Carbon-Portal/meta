5ace2dc6c6829ef21f1bf51533cb862255fa6dac
Jonathan Thiry <github@jonathanthiry.com>
2018-07-18 14:12:59 +0200

    Update GAWSIS url

--- src/main/js/labeling/views/AtmosphereStationFactory.jsx
+++ src/main/js/labeling/views/AtmosphereStationFactory.jsx
@@ -12,7 +12,7 @@ module.exports = function(StationMixins) {
 				<Inputs.Header txt="Identification" />
 				<Inputs.String {...this.getProps('hasLongName')} header="Station full name" />
 				<Inputs.String {...this.getProps('hasShortName')}
-					header="Station short name (short name trigram should be obtained from the GAWSIS system at http://gaw.empa.ch/gawsis/)" />
+					header="Station short name (short name trigram should be obtained from the GAWSIS system at https://gawsis.meteoswiss.ch/GAWSIS/)" />
 				<Inputs.TextArea {...this.getProps('hasMainPersonnelNamesList')} header="List of the names of the main personnel involved in ICOS station operation" />
 				<Inputs.String {...this.getProps('hasResponsibleInstitutionName')} header="Institution name responsible for the station"/>
 				<Inputs.TextArea {...this.getProps('hasAddress')} header="Postal address" />

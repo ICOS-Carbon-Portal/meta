b7b6a44ea5ef08c00a3f1e800815a470b077b1a8
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2019-01-17 18:37:59 +0100

    More test scenarios. Found a problem with the membership updates

--- src/test/scala/se/lu/nateko/cp/meta/test/icos/RdfDiffCalcTests.scala
+++ src/test/scala/se/lu/nateko/cp/meta/test/icos/RdfDiffCalcTests.scala
@@ -14,7 +14,7 @@ import se.lu.nateko.cp.meta.services.CpmetaVocab
 import se.lu.nateko.cp.meta.utils.rdf4j.Loading
 import org.scalatest.GivenWhenThen
 import se.lu.nateko.cp.meta.instanceserver.InstanceServer
-
+import RdfDiffCalcTests._
 
 class RdfDiffCalcTests extends FunSpec with GivenWhenThen{
 
@@ -22,64 +22,124 @@ class RdfDiffCalcTests extends FunSpec with GivenWhenThen{
 		Envri.ICOS -> EnvriConfig(null, null, null, new URI("http://test.icos.eu/resources/"))
 	)
 
+	type A = ATC.type
+	import TcConf.AtcConf.{makeId => aId}
+
+	val jane = Person[A]("Jane_Doe", aId("pers_0"), "Jane", "Doe", Some("jane.doe@icos-ri.eu"))
+	val airCpStation = CpMobileStation[A]("AIR1", aId("43"), "Airplane 1", "AIR1", None)
+
+	def atcInitSnap(pi: Person[A]): TcState[A] = {
+		val airTcStation = new TcStation[A](airCpStation, OneOrMorePis(pi))
+		new TcState[A](stations = Seq(airTcStation), roles = Nil, instruments = Nil)
+	}
+
 	describe("person name change"){
 
-		Given("starting with an empty state with no CP own statements")
+		Given("starting with an empty state with no own CP statements")
 
-		val (calc, reader, tcServer) = init(Nil, _ => Nil)
+		val state = init(Nil, _ => Nil)
 
 		When("an ATC-state snapshot with single station is inserted")
 
-		type A = ATC.type
-		import TcConf.AtcConf.makeId
+		val initUpdates = state.calc.calcDiff(atcInitSnap(jane)).toIndexedSeq
 
-		val jane = Person[A]("Jane_Doe", makeId("pers_0"), "Jane", "Doe", Some("jane.doe@icos-ri.eu"))
-		val cpStation = CpMobileStation[A]("AIR1", makeId("43"), "Airplane 1", "AIR1", None)
-		val theStation = new TcStation[A](cpStation, OneOrMorePis(jane))
-
-		val snap = new TcState[A](stations = Seq(theStation), roles = Nil, instruments = Nil)
-
-		val initUpdates = calc.calcDiff(snap).toIndexedSeq
-		Then("")
-		it("it results in expected sequence of RDF updates"){
+		it("Then it results in expected sequence of RDF updates"){
 			assert(initUpdates.forall(_.isAssertion))
-			assert(initUpdates.size > 10)
-			//initUpdates.foreach(println)
+			assert(initUpdates.size >= 13) //the number may change if metadata model changes
 		}
 
-		tcServer.applyAll(initUpdates)
+		state.tcServer.applyAll(initUpdates)
 
 		And("reading current TC state back produces expected value")
 
-		it("(has the PI, the station and the role)"){
-			val s = reader.getCurrentState[A]
+		it("(has the expected PI, the station and the role)"){
+			val s = state.reader.getCurrentState[A]
 			assert(s.stations.size === 1)
-			assert(s.stations.head === cpStation)
+			assert(s.stations.head === airCpStation)
 			assert(s.instruments.isEmpty)
 			assert(s.roles.size === 1)
 			val memb = s.roles.head
 			assert(memb.start.isEmpty) //init state was empty, so cannot know when the role was assumed first
 			assert(memb.stop.isEmpty) //just created, so cannot have ended
 			assert(memb.role.role === PI)
-			assert(memb.role.org === cpStation)
+			assert(memb.role.org === airCpStation)
 			assert(memb.role.holder === jane)
 		}
 
-		When("afterwards new snapshot comes with person last name changed")
+		When("afterwards a new snapshot comes with person's last name (and consequently cpId) changed")
 
 		val jane2 = jane.copy(cpId = "Jane_Smith", lName = "Smith")
-		val theStation2 = new TcStation[A](cpStation, OneOrMorePis(jane2))
-		val snap2 = new TcState[A](stations = Seq(theStation2), roles = Nil, instruments = Nil)
 
-		val nameUpdates = calc.calcDiff(snap2).toIndexedSeq
-		Then("")
-		it("only name-changing updates are applied"){
+		val nameUpdates = state.calc.calcDiff(atcInitSnap(jane2)).toIndexedSeq
+
+		it("Then only name-changing updates are applied"){
 			assert(nameUpdates.size === 2)
-			nameUpdates foreach println
+
+			val gist = nameUpdates.map{upd =>
+				upd.statement.getObject.stringValue -> upd.isAssertion
+			}.toMap
+
+			assert(gist === Map("Doe" -> false, "Smith" -> true))
 		}
 	}
 
-	def init(initTcState: Seq[CpTcState[_ <: TC]], cpOwn: RdfMaker => Seq[Statement]): (RdfDiffCalc, RdfReader, InstanceServer) = {
+	describe("PI change"){
+
+		Given("starting with a single station with single PI and no own CP statements")
+
+		val state = init(Nil, _ => Nil)
+		state.tcServer.applyAll(state.calc.calcDiff(atcInitSnap(jane)))
+
+		When("a new snapshot comes where the PI has changed")
+
+		val john = Person[A]("John_Brown", aId("pers_1"), "John", "Brown", Some("john.brown@icos-ri.eu"))
+		val piUpdates = state.calc.calcDiff(atcInitSnap(john)).toIndexedSeq
+		state.tcServer.applyAll(piUpdates)
+
+		it("Then previous PI's membership is ended and the new ones' is created and started"){
+			assert(piUpdates.size >= 11)
+			val membs = state.reader.getCurrentState[A].roles
+			assert(membs.size === 2)
+			val johnMemb = membs.find(_.role.holder == john).get
+			val janeMemb = membs.find(_.role.holder == jane).get
+			assert(johnMemb.start.isDefined)
+			assert(johnMemb.stop.isEmpty)
+			assert(janeMemb.start.isEmpty)
+			assert(janeMemb.stop.isDefined)
+		}
+
+	}
+
+	describe("CP takes over a definition of a metadata entity"){
+
+		Given("starting with a single station with single PI and no own CP statements")
+
+		val state = init(Nil, _ => Nil)
+		val initSnap = atcInitSnap(jane)
+		state.tcServer.applyAll(state.calc.calcDiff(initSnap))
+
+		When("CP creates a new person metadata and associates it with the exising TC person metadata")
+
+		val cpJane = Person[A]("Jane_CP", jane.tcId, "Jane", "CP", Some("jane.cp@icos-cp.eu"))
+		state.cpServer.addAll(state.maker.getStatements(cpJane))
+
+		val updates = state.calc.calcDiff(initSnap).toIndexedSeq //no change in the TC picture
+		state.tcServer.applyAll(updates)
+
+		it("Then arrival of an unchanged TC metadata snapshot results in deletion of TC's statements"){
+			val nOfPersonStats = state.maker.getStatements(jane).size
+			assert(updates.forall(!_.isAssertion))
+			assert(updates.size === nOfPersonStats)
+		}
+
+		it("And subsequent arrival of an unchanged TC metadata has no further effect"){
+			val updates2 = state.calc.calcDiff(initSnap).toIndexedSeq
+			updates2 foreach println
+			assert(updates2.isEmpty)
+		}
+	}
+
+	def init(initTcState: Seq[CpTcState[_ <: TC]], cpOwn: RdfMaker => Seq[Statement]): TestState = {
 		val repo = Loading.emptyInMemory
 		val factory = repo.getValueFactory
 		val vocab = new CpVocab(factory)
@@ -95,7 +155,7 @@ class RdfDiffCalcTests extends FunSpec with GivenWhenThen{
 
 		tcServer.addAll(initTcState.flatMap(getStatements(rdfMaker, _)))
 		val rdfReader = new RdfReader(cpServer, tcServer)
-		(new RdfDiffCalc(rdfMaker, rdfReader), rdfReader, tcServer)
+		new TestState(new RdfDiffCalc(rdfMaker, rdfReader), rdfReader, rdfMaker, tcServer, cpServer)
 	}
 
 	def getStatements[T <: TC](rdfMaker: RdfMaker, state: CpTcState[T]): Seq[Statement] = {
@@ -105,4 +165,9 @@ class RdfDiffCalcTests extends FunSpec with GivenWhenThen{
 		state.instruments.flatMap(rdfMaker.getStatements[T])
 	}
 
+}
+
+object RdfDiffCalcTests{
+	class TestState(val calc: RdfDiffCalc, val reader: RdfReader, val maker: RdfMaker,
+			val tcServer: InstanceServer, val cpServer: InstanceServer)
 }

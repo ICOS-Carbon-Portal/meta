4ba5d7b16d93a9fd110727632356740ef7d03b53
Jonathan Thiry <github@jonathanthiry.com>
2019-09-02 09:46:46 +0200

    [uploadgui] Add production metadata
    * [uploadgui] Add production metadata
    
    * [uploadgui] Improve form ui
    
    * [uploadgui] Display envri based timezone
    
    * [uploadgui] Cleanup timezone information

--- uploadgui/src/main/scala/se/lu/nateko/cp/meta/upload/Form.scala
+++ uploadgui/src/main/scala/se/lu/nateko/cp/meta/upload/Form.scala
@@ -3,7 +3,7 @@ package se.lu.nateko.cp.meta.upload
 import org.scalajs.dom
 import se.lu.nateko.cp.meta.core.data.Envri
 import se.lu.nateko.cp.meta.core.data.EnvriConfig
-import se.lu.nateko.cp.meta.{StationDataMetadata, SubmitterProfile, DataObjectDto, DocObjectDto, UploadDto, StaticCollectionDto}
+import se.lu.nateko.cp.meta.{StationDataMetadata, SubmitterProfile, DataObjectDto, DocObjectDto, UploadDto, StaticCollectionDto, DataProductionDto}
 
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 import scala.util.{Failure, Success, Try}
@@ -18,11 +18,14 @@ class Form(
 
 	val dataElements = new HtmlElements(".data-section")
 	val collectionElements = new HtmlElements(".collection-section")
+	val addProductionElements = new HtmlElements(".add-production-section")
+	val productionElements = new HtmlElements(".production-section")
 
 	val onFormTypeSelected: FormType => Unit = formType => {
 		dataElements.hide()
 		collectionElements.hide()
 		fileInput.enable()
+		productionElements.hide()
 
 		formType match {
 			case Data => dataElements.show()
@@ -35,27 +38,40 @@ class Form(
 		updateButton()
 	}
 
-	def submitAction(): Unit = typeControl.formType match {
-		case Data =>
-			for(dto <- dto; file <- fileInput.file; nRows <- nRowsInput.value; spec <- objSpecSelect.value) {
-				whenDone(Backend.tryIngestion(file, spec, nRows)){ _ =>
+	def submitAction(): Unit = {
+		dom.window.scrollTo(0, 0)
+		typeControl.formType match {
+			case Data =>
+				for(dto <- dataObjectDto; file <- fileInput.file; nRows <- nRowsInput.value; spec <- objSpecSelect.value) {
+					whenDone(Backend.tryIngestion(file, spec, nRows)){ _ =>
+						onUpload(dto, Some(file))
+					}
+				}
+			case Collection =>
+				for(dto <- staticCollectionDto) {
+					onUpload(dto, None)
+				}
+			case Document =>
+				for(dto <- documentObjectDto; file <- fileInput.file) {
 					onUpload(dto, Some(file))
 				}
-			}
-		case Collection =>
-			for(dto <- dto) {
-				onUpload(dto, None)
-			}
-		case Document =>
-			for(dto <- dto; file <- fileInput.file) {
-				onUpload(dto, Some(file))
-			}
+		}
 	}
-	val button = new SubmitButton("submitbutton", () => submitAction())
+	val submitButton = new Button("submitbutton", () => submitAction())
 	val updateButton: () => Unit = () => dto match {
-		case Success(_) => button.enable()
-		case Failure(err) => button.disable(err.getMessage)
+		case Success(_) => submitButton.enable()
+		case Failure(err) => submitButton.disable(err.getMessage)
 	}
+
+	val addProductionButton = new Button("addproductionbutton", () => {
+		addProductionElements.hide()
+		productionElements.show()
+	})
+
+	val removeProductionButton = new Button("removeproductionbutton", () => {
+		addProductionElements.show()
+		productionElements.hide()
+	})
 
 	val onLevelSelected: String => Unit = (level: String) =>
 		whenDone{
@@ -103,6 +119,13 @@ class Form(
 	val instrUriInput = new UriOptInput("instrumenturi", updateButton)
 	val timeIntevalInput = new TimeIntevalInput(acqStartInput, acqStopInput)
 
+	val creatorInput = new UriInput("creatoruri", updateButton)
+	val contributorsInput = new UriListInput("contributors", updateButton)
+	val hostOrganizationInput = new UriOptInput("hostorganisation", updateButton)
+	val commentInput = new TextOptInput("productioncomment", updateButton)
+	val creationDateInput = new InstantInput("creationdate", updateButton)
+	val sourcesInput = new HashOptListInput("sources", updateButton)
+
 	val collectionTitle = new TextInput("collectiontitle", updateButton)
 	val collectionDescription = new TextOptInput("collectiondescription", updateButton)
 	val collectionMembers = new UriListInput("collectionmembers", updateButton)
@@ -113,6 +136,22 @@ class Form(
 		case Document => documentObjectDto
 	}
 	private def isTypeSelected = if (typeControl.value.isEmpty) fail("No file type selected") else Success(())
+
+	def dataProductionDto: Try[Option[DataProductionDto]] = for(
+		creator <- creatorInput.value.withErrorContext("Creator");
+		contributors <- contributorsInput.value.withErrorContext("Contributors");
+		hostOrganization <- hostOrganizationInput.value.withErrorContext("Host organization");
+		comment <- commentInput.value.withErrorContext("Comment");
+		creationDate <- creationDateInput.value.withErrorContext("Creation date");
+		sources <- sourcesInput.value.withErrorContext("Sources")
+	) yield Some(DataProductionDto(
+		creator = creator,
+		contributors = contributors,
+		hostOrganization = hostOrganization,
+		comment = comment,
+		sources = sources,
+		creationDate = creationDate
+	))
 
 	def dataObjectDto: Try[DataObjectDto] = for(
 		file <- fileInput.file;
@@ -126,7 +165,8 @@ class Form(
 		acqInterval <- timeIntevalInput.value.withErrorContext("Acqusition time interval");
 		nRows <- nRowsInput.value.withErrorContext("Number of rows");
 		samplingHeight <- samplingHeightInput.value.withErrorContext("Sampling height");
-		instrumentUri <- instrUriInput.value.withErrorContext("Instrument URI")
+		instrumentUri <- instrUriInput.value.withErrorContext("Instrument URI");
+		production <- if(productionElements.areEnabled) dataProductionDto else Success(None)
 	) yield DataObjectDto(
 		hashSum = hash,
 		submitterId = submitter.id,
@@ -139,7 +179,7 @@ class Form(
 				samplingHeight = samplingHeight,
 				acquisitionInterval = acqInterval,
 				nRows = nRows,
-				production = None
+				production = production
 			)
 		),
 		isNextVersionOf = previousVersion.map(Left(_)),

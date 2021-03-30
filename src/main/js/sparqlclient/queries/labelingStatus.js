2f2165b5b5a898c20384491fc844e8c1e068fffb
Oleg Mirzov <oleg.mirzov@gmail.com>
2021-03-30 22:02:09 +0200

    [sparqlclient] Update the labeling-status query

--- src/main/js/sparqlclient/queries/labelingStatus.js
+++ src/main/js/sparqlclient/queries/labelingStatus.js
@@ -1,25 +1,35 @@
 export const labelingStatus = `PREFIX cpst: <http://meta.icos-cp.eu/ontologies/stationentry/>
+prefix cpmeta: <http://meta.icos-cp.eu/ontologies/cpmeta/>
 prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-SELECT ?stationTheme ?id
-(if(bound(?piName), ?piName, ?hoName) as ?name)
+SELECT ?stationTheme
+(coalesce(?tcId, ?hoId) as ?id)
+(coalesce(?tcName, ?hoName) as ?name)
 (if(
-  bound(?status),
-  if(
-    strstarts(?status, "STEP"),
-    ?status,
-    concat("STEP1", ?status)
-  ),
-  "AWAITINGSTEP1"
-  ) as ?labelingStatus)
+	bound(?status),
+	if(
+		strstarts(?status, "STEP"),
+		?status,
+		concat("STEP1", ?status)
+	),
+	"AWAITINGSTEP1"
+	) as ?labelingStatus)
+?lastModified
 FROM <http://meta.icos-cp.eu/ontologies/stationentry/>
 FROM <http://meta.icos-cp.eu/resources/stationentry/>
+FROM <http://meta.icos-cp.eu/resources/icos/>
 FROM NAMED <http://meta.icos-cp.eu/resources/stationlabeling/>
 WHERE {
-?s cpst:hasShortName ?id .
-?s cpst:hasLongName ?hoName .
-?s a [ rdfs:label ?stationTheme] .
-OPTIONAL{GRAPH <http://meta.icos-cp.eu/resources/stationlabeling/>{ ?s cpst:hasApplicationStatus ?status }}
-OPTIONAL{GRAPH <http://meta.icos-cp.eu/resources/stationlabeling/>{ ?s cpst:hasLongName ?piName}}
+	?s cpst:hasShortName ?hoId .
+	?s cpst:hasLongName ?hoName .
+	?s a [ rdfs:label ?stationTheme] .
+	OPTIONAL{GRAPH ?g { ?s cpst:hasApplicationStatus ?status }}
+	OPTIONAL{GRAPH ?g { ?s cpst:hasAppStatusDate ?lastModified }}
+	OPTIONAL{
+		?s cpst:hasProductionCounterpart ?psStr .
+		bind(iri(?psStr) as ?ps)
+		?ps cpmeta:hasStationId ?tcId .
+		?ps cpmeta:hasName ?tcName .
+	}
 }
 ORDER BY ?stationTheme ?id
 `;

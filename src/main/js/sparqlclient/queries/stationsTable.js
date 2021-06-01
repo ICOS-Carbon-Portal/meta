63659b57a5a9818d39d6fb030b0a7e824aba8567
Oleg Mirzov <oleg.mirzov@gmail.com>
2021-06-01 16:01:18 +0200

    [sparqlclient] Updated stationsTable 'lib' query

--- src/main/js/sparqlclient/queries/stationsTable.js
+++ src/main/js/sparqlclient/queries/stationsTable.js
@@ -1,27 +1,25 @@
 
-export const stationsTable = `PREFIX cpst: <http://meta.icos-cp.eu/ontologies/stationentry/>
+export const stationsTable = `PREFIX cpmeta: <http://meta.icos-cp.eu/ontologies/cpmeta/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX cpst: <http://meta.icos-cp.eu/ontologies/stationentry/>
 SELECT
-(IF(bound(?lat), str(?lat), "?") AS ?latstr)
-(IF(bound(?lon), str(?lon), "?") AS ?lonstr)
-(REPLACE(str(?class),"http://meta.icos-cp.eu/ontologies/stationentry/", "") AS ?themeShort)
-(str(?country) AS ?Country)
-(str(?sName) AS ?Short_name)
-(str(?lName) AS ?Long_name)
-(GROUP_CONCAT(?piLname; separator=";") AS ?PI_names)
-(str(?siteType) AS ?Site_type)
+(coalesce(?tcName, ?hoName) as ?Name)
+?Theme
+(coalesce(?tcClass, ?hoClass) as ?Class)
+(coalesce(?tcCountry, ?hoCountry) as ?Country)
 FROM <http://meta.icos-cp.eu/resources/stationentry/>
+FROM <http://meta.icos-cp.eu/ontologies/stationentry/>
+FROM <http://meta.icos-cp.eu/resources/icos/>
 WHERE {
-?s cpst:hasCountry ?country .
-?s cpst:hasShortName ?sName .
-?s cpst:hasLongName ?lName .
-?s cpst:hasSiteType ?siteType .
-?s cpst:hasPi ?pi .
-?pi cpst:hasLastName ?piLname .
-?s a ?class .
-OPTIONAL{?s cpst:hasLat ?lat } .
-OPTIONAL{?s cpst:hasLon ?lon } .
-OPTIONAL{?s cpst:hasSpatialReference ?spatRef } .
-OPTIONAL{?pi cpst:hasFirstName ?piFname } .
+	?s cpst:hasCountry ?hoCountry ; cpst:hasLongName ?hoName ; cpst:hasStationClass ?hoClass0 ; a/rdfs:label ?Theme .
+	optional{
+		?s cpst:hasProductionCounterpart ?prods .
+		bind(iri(?prods) as ?prod) .
+		optional{?prod cpmeta:hasName ?tcName }
+		optional{?prod cpmeta:hasStationClass ?tcClass}
+		optional{?prod cpmeta:countryCode ?tcCountry}
+	}
+	bind(if(contains(?hoClass0, 'Ass'), 'Associated', ?hoClass0) as ?hoClass)
+	filter exists {?s cpst:hasShortName []}
 }
-GROUP BY ?lat ?lon ?class ?country ?sName ?lName ?siteType
-ORDER BY ?themeShort ?sName`;
+order by ?Theme ?Name`;

5c1d1abea82ed7dea320ced7020f09e214ba10cb
Oleg Mirzov <oleg.mirzov@gmail.com>
2021-07-14 08:59:32 +0200

    [sparqlclient] Docs query: hide deprecated docs

--- src/main/js/sparqlclient/queries/existingDocuments.js
+++ src/main/js/sparqlclient/queries/existingDocuments.js
@@ -1,6 +1,7 @@
 export const existingDocuments = `prefix cpmeta: <http://meta.icos-cp.eu/ontologies/cpmeta/>
 select * where{
 	?doc a cpmeta:DocumentObject .
+	FILTER NOT EXISTS {[] cpmeta:isNextVersionOf ?doc}
 	?doc cpmeta:hasName ?fileName .
 	OPTIONAL{?doc cpmeta:hasCitationString ?citation}
 	OPTIONAL{?doc cpmeta:hasBiblioInfo ?bibinfo}

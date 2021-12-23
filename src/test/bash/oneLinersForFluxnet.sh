684f9a793a89bc95cb84e05a90e505ef85ff7dd8
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2021-12-23 16:32:03 +0100

    [winter2020][wip] Add IsICOS to the automatic CSV metadata

--- src/test/bash/oneLinersForFluxnet.sh
+++ src/test/bash/oneLinersForFluxnet.sh
@@ -14,15 +14,22 @@ sha256sum *.zip > hashes.txt
 
 for f in *.zip; do unzip -l $f | tail -n 3 | grep FLUXNET | awk '{print $4".zip", $2}'; done > dates.txt
 
+curl  -H "Accept: text/csv" -G --data-urlencode \
+"query=prefix cpmeta: <http://meta.icos-cp.eu/ontologies/cpmeta/> select ?id where{?s a cpmeta:ES ; cpmeta:hasStationId ?id}" \
+https://meta.icos-cp.eu/sparql | dos2unix | tail -n +2 | sort |
+awk 'BEGIN {OFS=","; print "StationID", "IsICOS"} {print $1, "-1"}' > icosStatus.txt
+
+
 join -j 2 lines.txt hashes.txt | 
 join -j 1 - dates.txt |
-awk 'BEGIN {OFS=","; print "StationID", "fileName", "Npoints", "hash", "creationDate"} {print substr($0,5,6), $1, ($2 - 1), $3, $4}' > \
+awk 'BEGIN {OFS=","; print "StationID", "fileName", "Npoints", "hash", "creationDate"} {print substr($0,5,6), $1, ($2 - 1), $3, $4}' |
+join -a 1 --header -t, -j 1 - icosStatus.txt > \
 autoInfo.csv
 
 #drought_data_hh.csv had CZ-Wet manuallyrenamed to CZ-wet,
 #column "hash" renamed to "prevHash", and commas replaced with |
 join -a 1 --header -t, -e "" -1 1 -2 8 \
--o 1.4,2.1,1.2,1.5,1.3,2.6,2.7,1.1,2.9,2.10,2.11,2.12,2.13,2.14,2.15,2.16,2.17,2.18,2.19,2.20,2.21,2.22,2.23,2.24,2.25 \
+-o 1.4,2.1,1.2,1.5,1.3,1.6,2.7,1.1,2.9,2.10,2.11,2.12,2.13,2.14,2.15,2.16,2.17,2.18,2.19,2.20,2.21,2.22,2.23,2.24,2.25 \
 autoInfo.csv drought_data_hh.csv > winter_data_hh.csv
 
 #change | back to ,

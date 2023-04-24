34def735e3678f0dc62c8f2ba08a52f0a295298e
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2023-04-24 10:38:32 +0200

    Make sure tests run without local config

--- src/test/scala/se/lu/nateko/cp/meta/test/services/sparql/regression/TestDb.scala
+++ src/test/scala/se/lu/nateko/cp/meta/test/services/sparql/regression/TestDb.scala
@@ -36,7 +36,8 @@ import se.lu.nateko.cp.meta.core.data.Envri
 
 class TestDb {
 
-	private val metaConf = se.lu.nateko.cp.meta.ConfigLoader.default
+	private val metaConf = se.lu.nateko.cp.meta.ConfigLoader.withDummyPasswords
+
 	val akkaConf = ConfigFactory.defaultReference()
 		.withValue("akka.loglevel", ConfigValueFactory.fromAnyRef("ERROR"))
 	private val system = ActorSystem("sparqlRegrTesting", akkaConf)

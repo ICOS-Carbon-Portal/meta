453ae472372d4b27f60520007d66081611d260c0
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2024-10-21 21:25:28 +0200

    [labeling] Add a disclaimer about outdated station information

--- src/main/js/labeling/views/NavBarFactory.jsx
+++ src/main/js/labeling/views/NavBarFactory.jsx
@@ -20,16 +20,28 @@ module.exports = function(WhoAmIStore) {
 				{this.props.piMode
 					? <h1>PI Information <small>for ICOS metadata</small></h1>
 					: <div>
-							<h1>Station labelling status</h1>
-							<p>The labelling status for ICOS stations.
-								You can read about <a target="_blank" href="https://www.icos-cp.eu/about/join-icos/process-stations">
-								labelling</a>,
-								look at the <a target="_blank" href="https://static.icos-cp.eu/share/stations/docs/labelling/labelingStateDiagram.svg">
-								state diagram for application process</a>,
-								and inspect the <a target="_blank" href="/labeling/labelingHistory.csv">
-								labelling &quot;history&quot;</a>.
-							</p>
+						<h1>Station labelling status</h1>
+						<div className="clearfix"></div>
+						<div className="alert alert-info" role="alert">
+							The labelling status for ICOS stations.
+							You can read about <a target="_blank" href="https://www.icos-cp.eu/about/join-icos/process-stations">
+							labelling</a>,
+							look at the <a target="_blank" href="https://static.icos-cp.eu/share/stations/docs/labelling/labelingStateDiagram.svg">
+							state diagram for application process</a>,
+							and inspect the <a target="_blank" href="/labeling/labelingHistory.csv">
+							labelling &quot;history&quot;</a>.
 						</div>
+						<div className="alert alert-danger d-flex align-items-center" role="alert">
+							<i className="fas fa-exclamation-triangle fa-2x me-2"></i>
+							<div>
+								Information on this page is likely to be outdated for the stations that have already been labeled.
+								Their metadata is maintained by the Thematic Centers who forward it to the Carbon Portal.
+								The latest station metadata can be found in the <a
+									href="https://www.icos-cp.eu/observations/station-network" target="_blank">station network table
+								</a> (and the station landing pages linked there)
+							</div>
+						</div>
+					</div>
 				}
 			</div>;
 		}

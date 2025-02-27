0661daf8b1f321f6392276c8058254446ae8e079
Valter Sundström <valter.sundstrom@gmail.com>
2025-02-27 14:00:30 +0100

    Fix test after rebase

--- src/test/scala/se/lu/nateko/cp/meta/test/services/sparql/index/IndexDataTest.scala
+++ src/test/scala/se/lu/nateko/cp/meta/test/services/sparql/index/IndexDataTest.scala
@@ -5,7 +5,7 @@ import org.eclipse.rdf4j.model.impl.SimpleValueFactory
 import org.scalatest.funsuite.AnyFunSuite
 import se.lu.nateko.cp.meta.api.CloseableIterator
 import se.lu.nateko.cp.meta.instanceserver.StatementSource
-import se.lu.nateko.cp.meta.services.sparql.magic.index.{IndexData, TripleStatement}
+import se.lu.nateko.cp.meta.services.sparql.magic.index.{IndexData, IndexUpdate}
 import se.lu.nateko.cp.meta.services.{CpVocab, CpmetaVocab}
 import se.lu.nateko.cp.meta.utils.rdf4j.Rdf4jStatement
 
@@ -21,27 +21,17 @@ class IndexDataTest extends AnyFunSuite {
 		// hence we can leave things unimplemented.
 		given StatementSource with
 			def getStatements(subject: IRI | Null, predicate: IRI | Null, obj: Value | Null): CloseableIterator[Statement] = ???
-					subject: IRI | Null,
-					predicate: IRI | Null,
-					obj: Value | Null
-				): CloseableIterator[Statement] = ???
 			def hasStatement(subject: IRI | Null, predicate: IRI | Null, obj: Value | Null): Boolean = ???
 
 		val statement = Rdf4jStatement(subject, vocab.hasName, factory.createLiteral("test name"))
 
 		// Insert hasName triple
-		data.processUpdate(statement, true, vocab)
-				Seq(TripleStatement(subject, vocab.hasName, factory.createLiteral("test name"), true)),
-				vocab
-			)
+		data.processUpdate(IndexUpdate(statement, true), vocab)
 		assert(data.objs.length == 1)
 		assert(data.getObjEntry(hash).fileName === Some("test name"))
 
 		// Remove it
-		data.processUpdate(statement, false, vocab)
-				Seq(TripleStatement(subject, vocab.hasName, factory.createLiteral("test name"), false)),
-				vocab
-			)
+		data.processUpdate(IndexUpdate(statement, false), vocab)
 		assert(data.getObjEntry(hash).fileName === None)
 		assert(data.objs.length == 1)
 	}

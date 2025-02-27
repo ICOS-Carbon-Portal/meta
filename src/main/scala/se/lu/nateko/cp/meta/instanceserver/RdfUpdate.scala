b51885febe9af840b0f0ca3cca7d51df60fe9042
Valter Sundström <valter.sundstrom@gmail.com>
2025-02-27 10:40:04 +0100

    Add case class for existing Rdf4jStatement and remove TripleStatement

--- src/main/scala/se/lu/nateko/cp/meta/instanceserver/RdfUpdate.scala
+++ src/main/scala/se/lu/nateko/cp/meta/instanceserver/RdfUpdate.scala
@@ -2,4 +2,7 @@ package se.lu.nateko.cp.meta.instanceserver
 
 import org.eclipse.rdf4j.model.Statement
 
+// TODO: Use Rdf4jStatement instead of Statement.
+// Would allow filtering earlier where we use RdfUpdate,
+// and get rid of IndexUpdate in se.lu.nateko.cp.meta.services.sparql.magic.index.IndexData
 case class RdfUpdate(statement: Statement, isAssertion: Boolean)

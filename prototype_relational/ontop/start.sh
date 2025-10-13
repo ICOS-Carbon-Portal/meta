a1775b77ad8db70ab4894f1653bdfaa401d56b6f
Valter Sundström <valter.sundstrom@gmail.com>
2025-10-13 14:04:18 +0200

    Use ontop 5.4.0 and remove unnecessary rewrites

--- prototype_relational/ontop/start.sh
+++ prototype_relational/ontop/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-../ontop-cli-5.3.0/ontop endpoint \
+../ontop-cli-5.4.0/ontop endpoint \
   --properties="ontop.properties" \
   --ontology="cpmeta.ttl" \
   --port=8080 \
--- prototype_relational/scripts/run_queries.py
+++ prototype_relational/scripts/run_queries.py
@@ -10,15 +10,7 @@ def print_query(query, index):
 
 
 def rewrite_query(query):
-    return query.replace("filter not exists", "FILTER NOT EXISTS") \
-            .replace("FILTER NOT EXISTS", "MINUS") \
-            .replace("BIND(EXISTS{[] cpmeta:isNextVersionOf ?dobj} AS ?hasNextVersion)",
-                      "OPTIONAL{?newer cpmeta:isNextVersionOf ?dobj}\n\tBIND(bound(?newer) AS ?hasNextVersion)") \
-            .replace("BIND(EXISTS{?dobj cpmeta:hasActualVariable [] } AS ?hasVarInfo)",
-                      "OPTIONAL{?dobj cpmeta:hasActualVariable ?varName}\n\tBIND(bound(?varName) AS ?hasVarInfo)") \
-            .replace("bind(exists{?station cpmeta:hasStationClass ?stClass} as ?isIcos)",
-                      "OPTIONAL{?station cpmeta:hasStationClass ?stClass}\n\tBIND(bound(?stClass) AS ?isIcos)") \
-            .replace("select (cpmeta:distinct_keywords() as ?keywords)", "select ?spec")
+    return query.replace("select (cpmeta:distinct_keywords() as ?keywords)", "select ?spec")
             # TODO: Rewrite distinct_keywords() to something proper instead of dropping it
 
 def run_query(query, host='http://localhost:65432/sparql'):
@@ -152,3 +144,4 @@ if __name__ == '__main__':
         input_file = 'unique_sparql.log'
 
     main(input_file, 'rewritten_queries.txt')
+    print("DONE")

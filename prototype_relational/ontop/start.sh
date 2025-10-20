f988e73620e3b034652711f29afc2c12c01f8e18
Valter Sundström <valter.sundstrom@gmail.com>
2025-10-20 12:58:55 +0200

    rename data_objects.obda to mappings.obda

--- prototype_relational/ontop/start.sh
+++ prototype_relational/ontop/start.sh
@@ -3,7 +3,8 @@
 
 ../ontop-cli-5.4.0/ontop endpoint \
   --properties="ontop.properties" \
+  --dev \
   --ontology="cpmeta.ttl" \
   --port=8080 \
   --cors-allowed-origins=* \
-  --mapping="mappings/data_objects.obda"
+  --mapping="mappings.obda"

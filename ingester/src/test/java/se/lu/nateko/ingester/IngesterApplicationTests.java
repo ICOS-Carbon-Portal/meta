cc45b82e9bac9bfaa5f6e1a0a2b6870318cee9c7
jonathanlindahl <jonathan.lindahl@nateko.lu.se>
2025-10-22 10:21:48 +0200

    mock repository for tests

--- ingester/src/test/java/se/lu/nateko/ingester/IngesterApplicationTests.java
+++ ingester/src/test/java/se/lu/nateko/ingester/IngesterApplicationTests.java
@@ -14,16 +14,34 @@ import org.json.JSONException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@SpringBootTest
+import se.lu.nateko.ingester.repository.IngestRepository;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class IngesterApplicationTests {
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@MockitoBean
+	private IngestRepository ingestRepository;
+
 	@ParameterizedTest
 	@MethodSource("provideJsons")
 	public void testThings(JsonNode jsonNode) throws JSONException, IOException, Exception {
+		Mockito.doNothing().when(ingestRepository.save(Mockito.any()));
 		List<String> values = new ArrayList<>();
 		if (jsonNode.has("fileName")) {
 			values.add(jsonNode.get("fileName").asText());

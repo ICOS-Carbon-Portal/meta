7ec88069721da6781f0362793c4637de25fafcf8
Valter Sundström <valter.sundstrom@gmail.com>
2025-11-24 15:38:53 +0100

    Add flatten lens for dataspec columns

--- prototype_relational/ontop/start.sh
+++ prototype_relational/ontop/start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# MAPPING=mapping/generated_all_mappings.obda
+# MAPPING=mapping/spec_and_hasName.obda
+MAPPING=mapping/edited_all.obda
 
-ONTOP_LOG_LEVEL=debug ../ontop-cli-5.4.0/ontop endpoint \
+ONTOP_LOG_LEVEL=DEBUG ../ontop-cli-5.4.0/ontop endpoint \
   --properties="ontop.properties" \
+  -l lenses.json \
   --dev \
   --ontology="cpmeta.ttl" \
   --port=8080 \
   --cors-allowed-origins=* \
-  --mapping="mapping/mappings.obda"
+  --mapping=$MAPPING

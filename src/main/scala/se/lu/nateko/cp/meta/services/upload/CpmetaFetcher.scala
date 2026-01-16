a7a3c85af2c61c57f9cfd871119f8b7a13096429
Jonathan Thiry <github@jonathanthiry.com>
2026-01-16 07:09:56 +0100

    Filter out data under moratorium in next versions (#333)

--- src/main/scala/se/lu/nateko/cp/meta/services/upload/CpmetaFetcher.scala
+++ src/main/scala/se/lu/nateko/cp/meta/services/upload/CpmetaFetcher.scala
@@ -14,7 +14,6 @@ import se.lu.nateko.cp.meta.utils.{Validated, containsEither, parseCommaSepList}
 
 import java.net.URI
 
-
 trait CpmetaReader:
 	import StatementSource.*
 	import RdfLens.{MetaConn, DobjConn, DocConn, ItemConn}
@@ -74,7 +73,7 @@ trait CpmetaReader:
 				uri = Some(cov.toJava)
 			)
 
-	def getSubmission(subm: IRI): (DobjConn | DocConn) ?=> Validated[DataSubmission] =
+	def getSubmission(subm: IRI): MetaConn ?=> Validated[DataSubmission] =
 		for
 			submitterUri <- getSingleUri(subm, metaVocab.prov.wasAssociatedWith)
 			submitter <- getOrganization(submitterUri)
@@ -239,6 +238,8 @@ trait CpmetaReader:
 				then getUriValues(next, metaVocab.dcterms.hasPart)
 				else Seq(next)
 			.filter(isComplete)
+			.filter: next =>
+				getSubmission(next).result.map(_.isUnderMoratorium).getOrElse(false)
 			.toIndexedSeq
 
 	def isPlainCollection[C <: ItemConn](item: IRI): C ?=> Boolean =

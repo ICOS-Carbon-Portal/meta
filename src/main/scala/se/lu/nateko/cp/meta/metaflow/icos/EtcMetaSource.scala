203ca07f2cd56b211f845f78ea3c442933c5ec83
Valter Sundström <valter.sundstrom@gmail.com>
2026-03-27 14:00:45 +0100

    Revert "Move HTTP fetching into companion object"
    This reverts commit cdf3ed9971cfb75ded0e5b9b65ede3818c24126c.

--- src/main/scala/se/lu/nateko/cp/meta/metaflow/icos/EtcMetaSource.scala
+++ src/main/scala/se/lu/nateko/cp/meta/metaflow/icos/EtcMetaSource.scala
@@ -41,11 +41,12 @@ import scala.concurrent.duration.DurationInt
 import scala.util.Failure
 
 
-class EtcMetaSource(conf: EtcConfig, vocab: CpVocab, fetchTsv: String => Future[Seq[EtcMetaSource.Lookup]])(using system: ActorSystem) extends TcMetaSource[ETC.type] {
+class EtcMetaSource(conf: EtcConfig, vocab: CpVocab)(using system: ActorSystem, mat: Materializer) extends TcMetaSource[ETC.type] {
 	import EtcMetaSource.*
 	import system.dispatcher
 
 	private val log = Logging.getLogger(system, this)
+	private val baseEtcApiUrl = Uri(conf.metaService.toString)
 
 	override def state: Source[State, () => Unit] = Source
 		.tick(35.seconds, 5.hours, NotUsed)
@@ -151,7 +152,12 @@ class EtcMetaSource(conf: EtcConfig, vocab: CpVocab, fetchTsv: String => Future[
 		fetchFromTsv(Types.meteosens, getSensorDeployment(stationLookup)).map(_.map(mergeInstrDeployments))
 	}
 
-	private def fetchAndParseTsv(tableType: String): Future[Seq[Lookup]] = fetchTsv(tableType)
+	private def fetchAndParseTsv[T](tableType: String): Future[Seq[Lookup]] = Http()
+		.singleRequest(HttpRequest(
+			uri = baseEtcApiUrl.withQuery(Uri.Query("type" -> tableType))
+		))
+		.flatMap(_.entity.toStrict(3.seconds))
+		.map(ent => parseTsv(ent.data))
 
 	private def fetchFromTsv[T](tableType: String, extractor: Lookup ?=> Validated[T]): Future[Validated[Seq[T]]] =
 		fetchAndParseTsv(tableType).map(lookups => Validated.sequence(lookups.map(extractor(using _))))
@@ -161,18 +167,6 @@ class EtcMetaSource(conf: EtcConfig, vocab: CpVocab, fetchTsv: String => Future[
 object EtcMetaSource{
 
 	type Lookup = Map[String, String]
-
-	def apply(conf: EtcConfig, vocab: CpVocab)(using system: ActorSystem, mat: Materializer): EtcMetaSource =
-		import system.dispatcher
-		val baseEtcApiUrl = Uri(conf.metaService.toString)
-		new EtcMetaSource(conf, vocab, tableType =>
-			Http()
-				.singleRequest(HttpRequest(
-					uri = baseEtcApiUrl.withQuery(Uri.Query("type" -> tableType))
-				))
-				.flatMap(_.entity.toStrict(3.seconds))
-				.map(ent => parseTsv(ent.data))
-		)
 	type E = ETC.type
 	private type EtcInstrument = TcInstrument[E]
 	private type EtcPerson = TcPerson[E]

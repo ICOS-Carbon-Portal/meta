691b49669db7412cbc6bb64fa9bf8d576127f684
Valter Sundström <valter.sundstrom@gmail.com>
2026-04-07 13:48:40 +0200

    Process streaks of assertions/retractions in order

--- src/main/scala/se/lu/nateko/cp/meta/persistence/QleverUpdateLogIngester.scala
+++ src/main/scala/se/lu/nateko/cp/meta/persistence/QleverUpdateLogIngester.scala
@@ -4,11 +4,12 @@ import scala.language.unsafeNulls
 
 import akka.actor.ActorSystem
 import akka.stream.Materializer
-import org.eclipse.rdf4j.model.IRI
+import org.eclipse.rdf4j.model.{IRI, Statement}
 import se.lu.nateko.cp.meta.api.CloseableIterator
 import se.lu.nateko.cp.meta.instanceserver.RdfUpdate
 import se.lu.nateko.cp.meta.services.sparql.QleverClient
 
+import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
 import scala.util.{Try, Using}
@@ -26,27 +27,32 @@ object QleverUpdateLogIngester:
 
 		val graphUri = writeContext.stringValue
 
-		def sendChunk(chunk: Seq[RdfUpdate]): Unit =
-			if chunk.nonEmpty then
-				val groups = toConsecutiveGroups(chunk)
-				for (isAssert, stmts) <- groups do
-					println(s"stmts: $stmts")
-					val fut = if isAssert then client.graphStoreAdd(graphUri, stmts)
-										else client.graphStoreRemove(graphUri, stmts)
-					Await.result(fut, 120.seconds)
+		def sendBatch(isAssertion: Boolean, stmts: Iterable[Statement]): Unit =
+			val fut = if isAssertion then client.graphStoreAdd(graphUri, stmts)
+						else client.graphStoreRemove(graphUri, stmts)
+			Await.result(fut, 120.seconds)
 
 		Using(updates): updates =>
 			if clearFirst then
 				Await.result(client.graphStoreClear(graphUri), 60.seconds)
-			for chunk <- updates.sliding(ChunkSize, ChunkSize) do
-				sendChunk(chunk)
 
-	private def toConsecutiveGroups(updates: Seq[RdfUpdate]): List[(Boolean, Vector[org.eclipse.rdf4j.model.Statement])] =
-		updates.foldLeft(List.empty[(Boolean, Vector[org.eclipse.rdf4j.model.Statement])]):
-			case ((isAssert, stmts) :: rest, update) if isAssert == update.isAssertion =>
-				(isAssert, stmts :+ update.statement) :: rest
-			case (groups, update) =>
-				(update.isAssertion, Vector(update.statement)) :: groups
-		.reverse
+			val buffer = ArrayBuffer.empty[Statement]
+			var currentIsAssertion = true
+			var bufferNonEmpty = false
+
+			for update <- updates do
+				if bufferNonEmpty && (update.isAssertion != currentIsAssertion || buffer.size >= ChunkSize) then
+					sendBatch(currentIsAssertion, buffer)
+					buffer.clear()
+					bufferNonEmpty = false
+
+				if !bufferNonEmpty then
+					currentIsAssertion = update.isAssertion
+					bufferNonEmpty = true
+
+				buffer += update.statement
+
+			if bufferNonEmpty then
+				sendBatch(currentIsAssertion, buffer)
 
 end QleverUpdateLogIngester

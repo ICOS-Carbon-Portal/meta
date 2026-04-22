7d456cd39ab5228352459b75d365fa96926d2270
Jonathan Thiry <github@jonathanthiry.com>
2026-04-22 09:21:18 +0200

    Add SPARQL link tests

--- src/test/scala/se/lu/nateko/cp/meta/test/InstOntoTests.scala
+++ src/test/scala/se/lu/nateko/cp/meta/test/InstOntoTests.scala
@@ -1,5 +1,6 @@
 package se.lu.nateko.cp.meta.test
 
+import org.eclipse.rdf4j.query.parser.sparql.SPARQLParser
 import org.scalatest.funspec.AnyFunSpec
 import se.lu.nateko.cp.meta.onto.{InstOnto, Onto}
 
@@ -9,6 +10,7 @@ class InstOntoTests extends AnyFunSpec{
 
 	val onto = new Onto(TestConfig.owlOnto)
 	val instOnto = new InstOnto(TestConfig.instServer, onto)
+	val stationClassUri = new URI(TestConfig.ontUri + "Station")
 
 	describe("getIndividual"){
 
@@ -29,6 +31,47 @@ class InstOntoTests extends AnyFunSpec{
 			val globalBox = new URI(TestConfig.instOntUri + "globalLatLonBox")
 			val range = instOnto.getRangeValues(stationClass, spatialCovProp)
 			assert(range.map(_.uri).contains(globalBox))
+		}
+	}
+
+	describe("getIndividualsSparql"){
+
+		it("includes basic query structure and common projected properties"){
+			val query = instOnto.getIndividualsSparql(stationClassUri, None)
+
+			assert(query.contains("SELECT ?s"))
+			assert(query.contains(s"?s rdf:type <$stationClassUri> ."))
+			assert(query.contains("ORDER BY ?s"))
+			assert(query.contains("rdfs:label"))
+			assert(query.contains("rdfs:comment"))
+		}
+
+		it("includes subject prefix filter when provided"){
+			val prefix = "http://meta.icos-cp.eu/"
+			val query = instOnto.getIndividualsSparql(stationClassUri, Some(prefix))
+
+			assert(query.contains(s"FILTER(STRSTARTS(STR(?s), \"$prefix\"))"))
+		}
+
+		it("omits subject prefix filter when not provided"){
+			val query = instOnto.getIndividualsSparql(stationClassUri, None)
+
+			assert(!query.contains("STRSTARTS(STR(?s),"))
+		}
+
+		it("escapes quotes and backslashes in subject prefix"){
+			val prefix = "http://meta.icos-cp.eu/path\\with\\slash/\"quoted\""
+			val query = instOnto.getIndividualsSparql(stationClassUri, Some(prefix))
+
+			assert(query.contains("path\\\\with\\\\slash"))
+			assert(query.contains("\\\"quoted\\\""))
+		}
+
+		it("produces a syntactically valid SPARQL query"){
+			val query = instOnto.getIndividualsSparql(stationClassUri, Some("http://meta.icos-cp.eu/"))
+
+			(new SPARQLParser).parseQuery(query, null)
+			assert(true)
 		}
 	}
 }
---
+++ src/test/scala/se/lu/nateko/cp/meta/test/routes/MetadataEntryRoutingTests.scala
@@ -0,0 +1,45 @@
+package se.lu.nateko.cp.meta.test.routes
+
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.headers.Host
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import eu.icoscp.envri.Envri
+import org.scalatest.funspec.AnyFunSpec
+import se.lu.nateko.cp.cpauth.core.PublicAuthConfig
+import se.lu.nateko.cp.meta.onto.{InstOnto, Onto}
+import se.lu.nateko.cp.meta.routes.{AuthenticationRouting, MetadataEntryRouting}
+import se.lu.nateko.cp.meta.test.TestConfig
+import se.lu.nateko.cp.meta.test.TestConfig.given
+import spray.json.*
+import spray.json.DefaultJsonProtocol.*
+
+import java.net.URI
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+class MetadataEntryRoutingTests extends AnyFunSpec with ScalatestRouteTest:
+
+	private val onto = new Onto(TestConfig.owlOnto)
+	private val instOnto = new InstOnto(TestConfig.instServer, onto)
+	private val authRouting = new AuthenticationRouting(Map.empty[Envri, PublicAuthConfig])
+	private val route = new MetadataEntryRouting(authRouting).singleOntoRoute(instOnto, Seq.empty)
+
+	private val classUri = new URI(TestConfig.ontUri + "Station")
+	private val encodedClassUri = URLEncoder.encode(classUri.toString, StandardCharsets.UTF_8)
+
+	describe("MetadataEntryRouting getIndividualsSparql"):
+
+		it("returns a query payload for a recognized ENVRI host"):
+			Get(s"/getIndividualsSparql?classUri=$encodedClassUri")
+				.withHeaders(Host("meta.icos-cp.eu")) ~> route ~> check:
+					assert(status === StatusCodes.OK)
+					val responseJson = responseAs[String].parseJson.asJsObject
+					val query = responseJson.fields("query").convertTo[String]
+					assert(query.contains("SELECT ?s"))
+					assert(query.contains("FILTER(STRSTARTS(STR(?s), \"http://meta.icos-cp.eu/\"))"))
+
+		it("returns bad request for unknown host"):
+			Get(s"/getIndividualsSparql?classUri=$encodedClassUri")
+				.withHeaders(Host("unknown.example.org")) ~> route ~> check:
+					assert(status === StatusCodes.BadRequest)
+					assert(responseAs[String].contains("Unexpected host"))

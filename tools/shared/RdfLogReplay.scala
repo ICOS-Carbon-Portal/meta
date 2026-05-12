c95b1a2b10c25e39718864cff01dea4b0ee2feff
Valter Sundström <valter.sundstrom@gmail.com>
2026-05-12 18:03:56 +0200

    ...

--- tools/shared/RdfLogReplay.scala
+++ tools/shared/RdfLogReplay.scala
@@ -8,14 +8,15 @@ import org.eclipse.rdf4j.model.ValueFactory
 import org.eclipse.rdf4j.repository.sail.SailRepository
 import se.lu.nateko.cp.meta.RdflogConfig
 import se.lu.nateko.cp.meta.persistence.postgres.PostgresRdfLog
-import se.lu.nateko.cp.meta.utils.rdf4j.Loading
+import org.eclipse.rdf4j.sail.memory.MemoryStore
 
 private val log = LoggerFactory.getLogger("tools.RdfLogReplay")
 
 def replayRdfLog(rdfLogConfig: RdflogConfig, factory: ValueFactory, logName: String): SailRepository = {
 	val rdfLog = PostgresRdfLog(logName, rdfLogConfig, factory)
 	log.info(s"$logName: replaying updates into in-memory repository")
-	val memRepo = Loading.emptyInMemory
+	val memRepo = new SailRepository(new MemoryStore)
+	memRepo.init()
 	Using.resource(rdfLog.updates) { updates =>
 		var replayed = 0
 		Using.resource(memRepo.getConnection) { conn =>

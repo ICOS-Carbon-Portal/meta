b305be69102f1176b0437adbf6d4d18c6ccdce10
Valter Sundström <valter.sundstrom@gmail.com>
2026-06-10 23:40:36 +0200

    Retry when datacite throttles

--- citation_populator/lib/citation_populator/throttle.ex
+++ citation_populator/lib/citation_populator/throttle.ex
@@ -18,6 +18,13 @@ defmodule CitationPopulator.Throttle do
     :ok
   end
 
+  @doc """
+  Pauses slot handout for the given time, on top of any pause already in
+  effect. Used when DataCite answers 429: the whole client is over the rate
+  limit, so every worker should back off, not just the one that hit it.
+  """
+  def backoff(ms), do: GenServer.cast(__MODULE__, {:backoff, ms})
+
   @impl true
   def init(nil), do: {:ok, System.monotonic_time(:millisecond)}
 
@@ -26,5 +33,10 @@ defmodule CitationPopulator.Throttle do
     now = System.monotonic_time(:millisecond)
     slot = max(now, next_at)
     {:reply, slot - now, slot + @interval_ms}
+  end
+
+  @impl true
+  def handle_cast({:backoff, ms}, next_at) do
+    {:noreply, max(next_at, System.monotonic_time(:millisecond) + ms)}
   end
 end

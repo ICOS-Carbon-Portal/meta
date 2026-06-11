99901de447b07cb115f31455b36ddb595d0062e1
Valter Sundström <valter.sundstrom@gmail.com>
2026-06-11 13:35:38 +0200

    Less concurrency

--- citation_populator/config/runtime.exs
+++ citation_populator/config/runtime.exs
@@ -10,4 +10,4 @@ config :citation_populator,
   virtuoso_password: System.get_env("VIRTUOSO_PASSWORD", "dba"),
   derived_citations_graph:
     System.get_env("DERIVED_CITATIONS_GRAPH", "http://meta.icos-cp.eu/derived/citations/"),
-  max_concurrency: String.to_integer(System.get_env("MAX_CONCURRENCY", "16"))
+  max_concurrency: String.to_integer(System.get_env("MAX_CONCURRENCY", "10"))

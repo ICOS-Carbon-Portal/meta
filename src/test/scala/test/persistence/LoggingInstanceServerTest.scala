bf3a1e0d9bbf556a18ceddd64d2c90c35e949413
Valter Sundström <valter.sundstrom@gmail.com>
2026-08-11 18:30:04 +0200

    Restore LoggingInstanceServerTest

--- src/test/scala/test/persistence/LoggingInstanceServerTest.scala
+++ src/test/scala/test/persistence/LoggingInstanceServerTest.scala
@@ -6,27 +6,57 @@ import org.eclipse.rdf4j.model.impl.SimpleValueFactory
 import org.eclipse.rdf4j.repository.sail.SailRepository
 import org.eclipse.rdf4j.sail.memory.MemoryStore
 import org.scalatest.funspec.AnyFunSpec
-import se.lu.nateko.cp.meta.instanceserver.{LoggingInstanceServer, Rdf4jInstanceServer, RdfUpdate}
-import se.lu.nateko.cp.meta.utils.rdf4j.accessEagerly
+import se.lu.nateko.cp.meta.instanceserver.{LoggingInstanceServer, Rdf4jInstanceServer}
+import se.lu.nateko.cp.meta.utils.rdf4j.*
 
 class LoggingInstanceServerTest extends AnyFunSpec:
-	private val factory = SimpleValueFactory.getInstance()
-	private val context = factory.createIRI("https://example.org/graph")
-	private val statement = factory.createStatement(
-		factory.createIRI("https://example.org/subject"),
-		factory.createIRI("https://example.org/predicate"),
-		factory.createLiteral("value")
-	)
 
-	describe("LoggingInstanceServer"):
-		it("uses the original LoggingInstanceServer behavior for configured graphs"):
-			val repo = SailRepository(MemoryStore())
-			repo.init()
-			val log = InMemoryRdfLog()
-			val server = LoggingInstanceServer(Rdf4jInstanceServer(repo, context), log)
+	val factory = SimpleValueFactory.getInstance()
+	val ctxt = factory.createIRI("http://www.icos-cp.eu/ontology/")
 
-			server.applyAll(Seq(RdfUpdate(statement, true)))().get
+	def makeUri(suff: String) = factory.createIRI(ctxt.stringValue, suff)
 
-			assert(log.updates.toSeq == Seq(RdfUpdate(statement, true)))
-			assert(repo.accessEagerly(_.hasStatement(statement, false, context)))
-			repo.shutDown()
+	describe("LoggingInstanceServer over Rdf4jInstanceServer"){
+
+		describe("after initializing with an empty in-memory log"){
+
+			val log = new InMemoryRdfLog()
+
+			val rdf4jRepo = SailRepository(MemoryStore())
+			rdf4jRepo.init()
+
+			val innerInstServer = new Rdf4jInstanceServer(rdf4jRepo, ctxt)
+			val loggingServer = new LoggingInstanceServer(innerInstServer, log)
+
+			val person = makeUri("Person")
+			val hasName = makeUri("hasName")
+
+			val person1 = loggingServer.makeNewInstance(person)
+			val person2 = loggingServer.makeNewInstance(person)
+			loggingServer.addInstance(person1, person)
+			loggingServer.addInstance(person2, person)
+			loggingServer.addPropertyValue(person1, hasName, factory.createLiteral("John"))
+			loggingServer.addPropertyValue(person2, hasName, factory.createLiteral("Jane"))
+
+			loggingServer.removeAll:
+				loggingServer.access: conn ?=>
+					conn.getStatements(person1, null, null).toIndexedSeq
+
+			it("logs all the RDF updates properly"){
+				val updates = log.updates.toSeq
+				assert(updates.map(_.isAssertion) === Seq(true, true, true, true, false, false))
+			}
+
+			it("updates the underlying Rdf4j repository correctly"){
+				val allStatements = rdf4jRepo.access(conn => conn.getStatements(null, null, null, false, ctxt)).toIndexedSeq
+				assert(allStatements.size === 2)
+			}
+
+			it("Rdf4jUtils RepositoryResult to Iterator conversion"){
+				val conn = rdf4jRepo.getConnection
+				def repRes = conn.getStatements(null, null, null, false, ctxt)
+				repRes.asPlainScalaIterator.toArray
+			}
+		}
+
+	}

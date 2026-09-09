68d687caa05b76b4be261808673465bb4e939c2c
Valter Sundström <valter.sundstrom@gmail.com>
2026-09-09 19:08:07 +0200

    Revert "Cache RDF subject reads per metadata request"
    This reverts commit 669c570f15e4c584626bb4ae1180c115ee228c02.

--- src/main/scala/services/linkeddata/UriSerializer.scala
+++ src/main/scala/services/linkeddata/UriSerializer.scala
@@ -30,7 +30,7 @@ import se.lu.nateko.cp.meta.api.*
 import se.lu.nateko.cp.meta.core.crypto.Sha256Sum
 import se.lu.nateko.cp.meta.core.data.JsonSupport.given
 import se.lu.nateko.cp.meta.core.data.*
-import se.lu.nateko.cp.meta.instanceserver.{SubjectCachingTriplestoreConnection, TriplestoreConnection, StatementSource}
+import se.lu.nateko.cp.meta.instanceserver.{TriplestoreConnection, StatementSource}
 import se.lu.nateko.cp.meta.services.CpVocab
 import se.lu.nateko.cp.meta.services.MetadataException
 import se.lu.nateko.cp.meta.services.attribution.AttributionProvider
@@ -162,17 +162,10 @@ class Rdf4jUriSerializer(
 
 
 	private def fetchStaticObj(hash: Sha256Sum)(using Envri): Validated[StaticObject] =
-		val startedAt = System.nanoTime()
-		val conn = SubjectCachingTriplestoreConnection(server.getConnection())
-		try
+		server.access: conn ?=>
 			val objIri = vocab.getStaticObject(hash)
 			given GlobConn = RdfLens.global(using conn)
 			objReader.fetchStaticObject(objIri)
-		finally
-			conn.profileSummary.foreach: summary =>
-				val elapsedMillis = (System.nanoTime() - startedAt) / 1000000
-				log.info(s"Object RDF-read profile for $hash: elapsed=${elapsedMillis}ms, $summary")
-			conn.close()
 
 
 	private def fetchStaticColl(hash: Sha256Sum)(using Envri): Validated[StaticCollection] =
@@ -209,13 +202,9 @@ class Rdf4jUriSerializer(
 		yield PersonExtra(pers, roles)
 
 	private def access[T, C <: TriplestoreConnection](lensV: Validated[RdfLens[C]])(reader: C ?=> Validated[T]): Validated[T] =
-		accessCached:
+		server.access:
 			lensV.flatMap: lens =>
 				reader(using lens)
-
-	private def accessCached[T](read: (TriplestoreConnection & SparqlRunner) ?=> T): T =
-		val conn = SubjectCachingTriplestoreConnection(server.getConnection())
-		try read(using conn) finally conn.close()
 
 	private def accessMeta[T](reader: MetaConn ?=> Validated[T])(using Envri): Validated[T] =
 		access(lenses.metaInstanceLens)(reader)

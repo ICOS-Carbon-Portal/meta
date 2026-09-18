c272f45d7fa5f23432ae6b4839f2cffa8b89c3ee
Valter Sundström <valter.sundstrom@gmail.com>
2026-09-18 16:26:06 +0200

    WIP: Introduce intermediate LandingPage type

---
+++ src/main/scala/services/linkeddata/LandingPageAssembler.scala
@@ -0,0 +1,51 @@
+package se.lu.nateko.cp.meta.services.linkeddata
+
+import eu.icoscp.envri.Envri
+import se.lu.nateko.cp.meta.api.{OrganizationExtra, PersonExtra, StatisticsClient}
+import se.lu.nateko.cp.meta.core.data.{Instrument, Organization, StaticCollection, StaticObject, Station}
+import se.lu.nateko.cp.meta.views.ResourceViewInfo
+
+import scala.concurrent.Future
+
+/**
+ * Adds request-scoped, non-RDF information to metadata read through the meta connection.
+ *
+ * The caller supplies the metadata request. This class deliberately performs its statistics
+ * requests only after that request has completed, so it never owns or extends an RDF connection
+ * lifetime.
+ */
+final class LandingPageAssembler(statistics: StatisticsClient):
+	def staticObject(metadata: StaticObject, warnings: Seq[String])(using Envri): Future[LandingPage.StaticObjectPage] =
+		import statistics.executionContext
+		for
+			downloads <- statistics.getObjDownloadCount(metadata)
+			previews <- statistics.getPreviewCount(metadata.hash)
+		yield LandingPage.StaticObjectPage(
+			metadata,
+			LandingPageMetrics(downloads, previews),
+			warnings
+		)
+
+	def staticCollection(metadata: StaticCollection, warnings: Seq[String])(using Envri): Future[LandingPage.StaticCollectionPage] =
+		import statistics.executionContext
+		statistics.getCollDownloadCount(metadata.res).map: downloads =>
+			LandingPage.StaticCollectionPage(
+				metadata,
+				LandingPageMetrics(downloads, None),
+				warnings
+			)
+
+	def station(metadata: OrganizationExtra[Station], warnings: Seq[String]): LandingPage.StationPage =
+		LandingPage.StationPage(metadata, warnings)
+
+	def organization(metadata: OrganizationExtra[Organization], warnings: Seq[String]): LandingPage.OrganizationPage =
+		LandingPage.OrganizationPage(metadata, warnings)
+
+	def instrument(metadata: Instrument, warnings: Seq[String]): LandingPage.InstrumentPage =
+		LandingPage.InstrumentPage(metadata, warnings)
+
+	def person(metadata: PersonExtra, warnings: Seq[String]): LandingPage.PersonPage =
+		LandingPage.PersonPage(metadata, warnings)
+
+	def genericResource(metadata: ResourceViewInfo, warnings: Seq[String] = Nil): LandingPage.GenericResourcePage =
+		LandingPage.GenericResourcePage(metadata, warnings)
--- src/main/scala/services/upload/PageContentMarshalling.scala
+++ src/main/scala/services/upload/PageContentMarshalling.scala
@@ -7,82 +7,65 @@ import akka.http.scaladsl.marshalling.{Marshaller, Marshalling, ToEntityMarshall
 import akka.http.scaladsl.model.*
 import eu.icoscp.envri.Envri
 import play.twirl.api.Html
-import se.lu.nateko.cp.meta.api.StatisticsClient
 import se.lu.nateko.cp.meta.core.CommonJsonSupport.WithErrors
-import se.lu.nateko.cp.meta.core.HandleProxiesConfig
 import se.lu.nateko.cp.meta.core.data.JsonSupport.given
 import se.lu.nateko.cp.meta.core.data.{EnvriConfig, StaticCollection, StaticObject}
-import se.lu.nateko.cp.meta.services.CpVocab
+import se.lu.nateko.cp.meta.services.linkeddata.{LandingPage, LandingPageAssembler, LandingPageRenderer}
 import se.lu.nateko.cp.meta.utils.{Validated, getStackTrace}
-import se.lu.nateko.cp.meta.views.LandingPageExtras
 import spray.json.*
-import views.html.{CollectionLandingPage, LandingPage, MessagePage}
+import views.html.MessagePage
 
 import java.util.concurrent.ExecutionException
 import scala.concurrent.{ExecutionContext, Future}
 
-class PageContentMarshalling(handleProxies: HandleProxiesConfig, statisticsClient: StatisticsClient):
+class PageContentMarshalling(
+	landingPages: LandingPageAssembler,
+	renderer: LandingPageRenderer
+):
 
 	import PageContentMarshalling.*
 
-	def staticObjectMarshaller (using Envri, EnvriConfig, CpVocab) : ToResponseMarshaller[() => Validated[StaticObject]] =
-		import statisticsClient.executionContext
-		val template: PageTemplate[StaticObject] = (obj, errors) =>
-			for(
-				dlCount <- statisticsClient.getObjDownloadCount(obj);
-				previewCount <- statisticsClient.getPreviewCount(obj.hash)
-			) yield {
-				val extras = LandingPageExtras(dlCount, previewCount, errors)
-				LandingPage(obj, extras, handleProxies)
-			}
-		makeMarshaller(template, messagePage("Data object not found", _))
-
+	def staticObjectMarshaller(using Envri, EnvriConfig): ToResponseMarshaller[() => Validated[StaticObject]] =
+		makeLandingPageMarshaller(
+			landingPages.staticObject,
+			messagePage("Data object not found", _)
+		)
 
 	def staticCollectionMarshaller(using Envri, EnvriConfig): ToResponseMarshaller[() => Validated[StaticCollection]] =
-		import statisticsClient.executionContext
-		val template: PageTemplate[StaticCollection] = (coll, errors) =>
-			for(dlCount <- statisticsClient.getCollDownloadCount(coll.res))
-			yield {
-				val extras = LandingPageExtras(dlCount, None, errors)
-				CollectionLandingPage(coll, extras, handleProxies)
-			}
-		makeMarshaller(template, messagePage("Collection not found", _))
+		makeLandingPageMarshaller(
+			landingPages.staticCollection,
+			messagePage("Collection not found", _)
+		)
 
 
 	// TODO Either allow fetching JSON without looking up download/preview stats, or include the stats in the JSON
-	private def makeMarshaller[T: JsonWriter](
-		templateFetcher: (T, ErrorList) => Future[Html],
+	private def makeLandingPageMarshaller[T: JsonWriter, P <: LandingPage](
+		assemble: (T, ErrorList) => Future[P],
 		notFoundPage: ErrorList => Html,
-	): ToResponseMarshaller[() => Validated[T]] = {
-
-		def fetchHtmlMaker(itemV: Validated[T])(using ExecutionContext): Future[HttpCharset => HttpResponse] = itemV.result match
-			case Some(item) =>
-				templateFetcher(item, itemV.errors).map: html =>
-					charset => HttpResponse(entity = getHtml(html, charset))
-
-			case None =>
-				Future.successful:
-					charset => HttpResponse(StatusCodes.NotFound, entity = getHtml(notFoundPage(itemV.errors), charset))
-
-
+	)(using Envri, EnvriConfig): ToResponseMarshaller[() => Validated[T]] =
 		Marshaller {exeCtxt => producer =>
 			given ExecutionContext = exeCtxt
 			val itemV: Validated[T] = producer()
-			for (
-				htmlMaker <- fetchHtmlMaker(itemV)
-			) yield List(
-				WithOpenCharset(MediaTypes.`text/html`, htmlMaker),
-				WithFixedContentType(ContentTypes.`application/json`, () => getJson(itemV))
-			)
+			fetchHtmlMaker(itemV, assemble, notFoundPage).map: htmlMaker =>
+				List(
+					WithOpenCharset(MediaTypes.`text/html`, htmlMaker),
+					WithFixedContentType(ContentTypes.`application/json`, () => getJson(itemV))
+				)
 		}
-	}
+
+	private def fetchHtmlMaker[T, P <: LandingPage](
+		itemV: Validated[T], assemble: (T, ErrorList) => Future[P], notFoundPage: ErrorList => Html
+	)(using ExecutionContext, Envri, EnvriConfig): Future[HttpCharset => HttpResponse] = itemV.result match
+		case Some(item) => assemble(item, itemV.errors).map: page =>
+			val html = renderer.render(page)
+			charset => HttpResponse(entity = getHtml(html, charset))
+		case None => Future.successful: charset =>
+			HttpResponse(StatusCodes.NotFound, entity = getHtml(notFoundPage(itemV.errors), charset))
 end PageContentMarshalling
 
 object PageContentMarshalling:
 
 	type ErrorList = Seq[String]
-	type PageTemplate[T] = (T, ErrorList) => Future[Html]
-
 	given twirlHtmlEntityMarshaller: ToEntityMarshaller[Html] = Marshaller(
 		_ => html => Future.successful(
 			WithOpenCharset(MediaTypes.`text/html`, getHtml(html, _)) :: Nil
